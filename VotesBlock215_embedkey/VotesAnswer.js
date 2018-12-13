4137817a0d16a8e4ca2d5d3f19a43d8f91c64471
likhuta <45772424+likhuta@users.noreply.github.com>
2018-12-13 09:53:28 +0300

    Add files via upload
    Вопрос о видимости key при передаче props вложенном   компоненту

---
+++ VotesBlock215_embedkey/VotesAnswer.js
@@ -0,0 +1,22 @@
+﻿var VotesAnswer = React.createClass({
+ 
+
+  displayName: 'VotesAnswer',
+
+  propTypes: {
+    count: React.PropTypes.number.isRequired,
+    text: React.PropTypes.string.isRequired,
+  },
+
+  render: function() {
+    console.log( 'VotesAnswer',this.props);
+    //console.log(this.props.key)
+   // передается сюда key  вместе с props?
+    return React.DOM.div( {className:'VotesBlockAnswer'},
+      React.DOM.span({className:'Count'},this.props.count),
+      React.DOM.span({className:'Text'},this.props.text),
+    );
+
+  },
+
+});
---
+++ VotesBlock215_embedkey/VotesBlock.js
@@ -0,0 +1,33 @@
+﻿var VotesBlock = React.createClass({
+
+  displayName: 'VotesBlock',
+
+  propTypes: {
+    question: React.PropTypes.string.isRequired,
+    answers:React.PropTypes.arrayOf(
+      React.PropTypes.shape({
+        code: React.PropTypes.number.isRequired,
+        count: React.PropTypes.number.isRequired,
+        text: React.PropTypes.string.isRequired,
+      })
+    )
+  },
+
+  render: function() {
+    console.log('VotesBlock', this.props);
+
+    var answersCode=this.props.answers.map( v =>
+      // в компонент VotesAnswer передается key:v.code . его по идее
+      // я могу видеть в props-ах VotesAnswer
+      // но его там нет. почему?
+      React.createElement(VotesAnswer, {key:v.code, text:v.text, count:v.count} )
+    );
+    console.log('answersCode' ,answersCode);
+
+    return React.DOM.div( {className:'VotesBlock'}, 
+      React.createElement(VotesQuestion, {question:this.props.question} ),
+      React.DOM.div( {className:'Answers'}, answersCode ),
+    );
+  },
+
+});
